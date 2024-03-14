e4d26a75c1f718cceedc290ab886b389fc897cfa
Mehdipoladrag <mehdipoladrag1382@gmail.com>
2024-03-14 06:53:08 +0330

    group tasks

--- eshop/notifications/tasks.py
+++ eshop/notifications/tasks.py
@@ -1,11 +1,31 @@
+import time
+from celery import group
 from celery import shared_task
 
 
 @shared_task
-def send_discount_emails():
-    pass
+def task_1(queue='celery'):
+    time.sleep(3)
+    return
 
 
 @shared_task
-def process_data_for_ml():
-    pass
+def task_2(queue='celery:1'):
+    time.sleep(3)
+    return
+
+
+@shared_task
+def task_3(queue='celery:2'):
+    time.sleep(3)
+    return
+
+
+@shared_task
+def task_4(queue='celery:3'):
+    time.sleep(3)
+    return
+
+
+group_tasks = group(task_1.s(),task_2.s(),task_3.s(),task_4.s())
+group_tasks.apply_async()
