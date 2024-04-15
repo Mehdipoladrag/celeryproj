b9c421143eeb3c721500bc5007eb3125e34f4cfd
Mehdipoladrag <mehdipoladrag1382@gmail.com>
2024-04-15 21:59:27 +0330

    configuration RabbitMQ part 1

--- eshop/notifications/tasks.py
+++ eshop/notifications/tasks.py
@@ -1,10 +1,9 @@
 import time
-from celery import group
 from celery import shared_task
 
 
 @shared_task
-def task_1(queue='celery'):
+def task_1(rate_limit='1/m'):
     time.sleep(3)
     return
 
@@ -27,5 +26,10 @@ def task_4(queue='celery:3'):
     return
 
 
+"""
+from celery import group, chain
 group_tasks = group(task_1.s(),task_2.s(),task_3.s(),task_4.s())
 group_tasks.apply_async()
+chain_tasks = chain(task_1.s(),task_2.s(), task_3.s(), task_4.s())
+chain_tasks.apply_async()
+"""
