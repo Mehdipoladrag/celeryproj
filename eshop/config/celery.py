ce95471a7287855c0974944bbb6f809262c189e8
Mehdipoladrag <mehdipoladrag1382@gmail.com>
2024-04-16 13:58:54 +0330

    configuration RabbitMQ part 2 and Create some tasks

--- eshop/config/celery.py
+++ eshop/config/celery.py
@@ -1,6 +1,8 @@
 import os
 from celery import Celery
 from kombu import Exchange, Queue
+import time
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
@@ -15,6 +17,43 @@ app.conf.task_acks_late = True
 app.conf.task_default_priority = 5
 app.conf.worker_prefetch_multiplier = 1
 app.conf.worker_concurrency = 1
+
+
+@app.task(queue='tasks')
+def task_1():
+    time.sleep(3)
+    return
+
+
+@app.task(queue='tasks')
+def task_2():
+    time.sleep(3)
+    return
+
+
+@app.task(queue='tasks')
+def task_3():
+    time.sleep(3)
+    return
+
+
+@app.task(queue='tasks')
+def task_4():
+    time.sleep(3)
+    return
+
+
+def handle_tasks():
+    task_2.apply_async(priority=2)
+    task_4.apply_async(priority=4)
+    task_1.apply_async(priority=1)
+    task_3.apply_async(priority=3)
+    task_2.apply_async(priority=2)
+    task_4.apply_async(priority=4)
+    task_1.apply_async(priority=1)
+    task_3.apply_async(priority=3)
+
+
 # app.conf.task_routes = {
 #     'notifications.tasks.send_discount_emails': {'queue': 'queue1'},
 #     'notifications.tasks.process_data_for_ml': {'queue': 'queue2'},
@@ -26,5 +65,6 @@ app.conf.worker_concurrency = 1
 #     'sep': ':',
 #     'queue_order_strategy': 'priority',
 # }
+
 
 app.autodiscover_tasks()
