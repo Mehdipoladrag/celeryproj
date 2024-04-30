af39a524ab5ecb6dd85bd8890c4a9e89f702fe6e
Mehdipoladrag <mehdipoladrag1382@gmail.com>
2024-04-30 19:20:08 +0330

    Time limit && Hard time limit (5s) exceeded for notifications.tasks.email.send_email

--- eshop/notifications/tasks/email.py
+++ eshop/notifications/tasks/email.py
@@ -1,6 +1,7 @@
-from celery import shared_task, Task
+from config.celery_config import app
+import time
 
-@shared_task(queue='tasks')
+@app.task(queue='tasks', time_limit=5)
 def send_email():
-    raise ConnectionError("Connection lost ....")
-    #print("email has been successfully sent")
+    time.sleep(6)
+    return "(*Email has been sent to user successfully*)"
