33dfd7a239ba18734524de38963d92c6135f8e52
Mehdipoladrag <mehdipoladrag1382@gmail.com>
2024-04-30 20:09:02 +0330

    Timeout

--- eshop/notifications/tasks/email.py
+++ eshop/notifications/tasks/email.py
@@ -1,7 +1,17 @@
 from config.celery_config import app
 import time
 
-@app.task(queue='tasks', time_limit=5)
+@app.task(queue='tasks', time_limit=10)
 def send_email():
     time.sleep(6)
     return "(*Email has been sent to user successfully*)"
+
+
+
+def send_mail():
+    result = send_email.delay()
+    try:
+        task_result = result.get(timeout=4)
+
+    except TimeoutError:
+        print('Task Timed out')
